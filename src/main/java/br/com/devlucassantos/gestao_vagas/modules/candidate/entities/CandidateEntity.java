c351f8b8f044e3be615cc3b50be1800fd76f1066
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-27 23:18:37 -0300

    Criando a tabela de candidato

--- src/main/java/br/com/devlucassantos/gestao_vagas/modules/candidate/entities/CandidateEntity.java
+++ src/main/java/br/com/devlucassantos/gestao_vagas/modules/candidate/entities/CandidateEntity.java
@@ -1,15 +1,24 @@
 package br.com.devlucassantos.gestao_vagas.modules.candidate.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+@Entity(name = "candidate")
 @Data
 public class CandidateEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String name;
 
@@ -21,6 +30,9 @@ public class CandidateEntity {
     private String password;
     private String description;
     private String curriculum;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 
 
 }
