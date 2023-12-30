2dd362234092935c91a0b5a382147b42ba26f770
LucasSantosp09 <lucassantos.p09@gmail.com>
2023-12-29 22:28:38 -0300

    Entidade Job

---
+++ src/main/java/br/com/devlucassantos/gestao_vagas/modules/company/entities/CompanyEntity.java
@@ -0,0 +1,32 @@
+package br.com.devlucassantos.gestao_vagas.modules.company.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.validator.constraints.Length;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity(name = "company")
+@Data
+public class CompanyEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String username;
+    @Email(message = "O campo [email] deve conter um e-mail válido")
+    private String email;
+    @Length(min = 10, max = 100)
+    private String password;
+    private String website;
+    private String name;
+    private String description;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+}
