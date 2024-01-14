fe23d60beb252d41fa570e2b101dd5349338f705
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-01-14 19:55:44 -0300

    Criando token do candidato

--- src/main/java/br/com/devlucassantos/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
+++ src/main/java/br/com/devlucassantos/gestao_vagas/modules/candidate/repositories/CandidateRepository.java
@@ -10,4 +10,5 @@ import java.util.UUID;
 @Repository
 public interface CandidateRepository extends JpaRepository <CandidateEntity, UUID> {
     Optional<CandidateEntity> findByUsernameOrEmail(String username, String email);
+    Optional<CandidateEntity> findByUsername(String username);
 }
