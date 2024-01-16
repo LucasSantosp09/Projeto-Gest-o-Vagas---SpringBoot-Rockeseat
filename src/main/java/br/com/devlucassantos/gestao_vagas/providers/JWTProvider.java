b6cefd3f7f9fa44b66577cf8f8d0fa228d5a85a8
LucasSantosp09 <lucassantos.p09@gmail.com>
2024-01-15 23:38:51 -0300

    Refatorando Filtro de Company

--- src/main/java/br/com/devlucassantos/gestao_vagas/providers/JWTProvider.java
+++ src/main/java/br/com/devlucassantos/gestao_vagas/providers/JWTProvider.java
@@ -3,6 +3,7 @@ package br.com.devlucassantos.gestao_vagas.providers;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -10,18 +11,18 @@ import org.springframework.stereotype.Service;
 public class JWTProvider {
     @Value("${security.token.secret}")
     private String secretKey;
-        public String validateToken (String token) {
+        public DecodedJWT validateToken (String token) {
             token = token.replace("Bearer ", "");
 
             Algorithm algorithm = Algorithm.HMAC256(secretKey);
 
             try{
-                var subjetct = JWT.require(algorithm).build().verify(token).getSubject();
+                var tokenDecoded = JWT.require(algorithm).build().verify(token);
 
-                return subjetct;
+                return tokenDecoded;
             } catch (JWTVerificationException ex) {
                 ex.printStackTrace();
-                return "";
+                return null;
             }
 
 
--- src/main/java/br/com/devlucassantos/gestao_vagas/security/SecurityConfig.java
+++ src/main/java/br/com/devlucassantos/gestao_vagas/security/SecurityConfig.java
@@ -33,6 +33,8 @@ public class SecurityConfig {
                 .addFilterBefore(secutiryFilter, BasicAuthenticationFilter.class);
 
 
+
+
     ;
         return http.build();
     }
